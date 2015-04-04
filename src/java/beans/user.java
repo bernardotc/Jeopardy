01897d5460373e3a085001b914af27b8bf373208
Bernardo Trevino <bernardotc94@gmail.com>
2015-04-04 16:04:19 -0600

    Se creo user como un javabeans, userHandler para acceder a la DB y controller como el server (Falta por desarrollarse). Se tiene el diagrama de la base de datos y la base de datos en mysql (fuera del proyecto).

---
+++ src/java/beans/user.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author bernardot
+ */
+public class user {
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String correo;
+    private String password;
+
+    public user() {
+    }
+
+    public user(String nombre, String apellido, String usuario, String correo, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
